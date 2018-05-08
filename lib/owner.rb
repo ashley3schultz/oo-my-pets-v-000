224f2dfd19de1de4c4610d0e4e031360a33282cd
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-08 01:14:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,7 +35,7 @@ class Owner
   
   def buy_fish(name)
     new_fish = Fish.new(name)
-    new_pets = @pets 
+    new_pets = pets 
     new_pets[:fishes] << new_fish
     @pets = new_pets
     binding.pry
