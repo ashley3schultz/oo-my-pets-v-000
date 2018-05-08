10144f3aa234d191867d190591e35114072f079a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-08 01:28:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,4 +42,9 @@ class Owner
     new_fish = Fish.new(name)
     @pets[:fishes] << new_fish
   end
+  
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
 end
