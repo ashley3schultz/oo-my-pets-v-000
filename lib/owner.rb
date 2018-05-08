2accfbfd8ef02af4a36b4a63c927b84cdbd0dbfe
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-08 00:29:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :species
+  
+  @@all = [] 
+  
+  def initialize(species)
+    @species = species
+    @@all << self
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end
+  
+  def self.count 
+    @@all.size 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
 end
